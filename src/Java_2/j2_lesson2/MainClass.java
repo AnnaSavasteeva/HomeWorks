6a05cad157321b994caf997e3fdf59deb1e01cf5
AnnaSavasteeva <anna.savasteeva@gmail.com>
2021-05-29 21:27:59 +0300

    Homework, Java-2: lesson 2, exceptions Финальная доработка ДЗ.

--- src/Java_2/j2_lesson2/MainClass.java
+++ src/Java_2/j2_lesson2/MainClass.java
@@ -1,9 +1,9 @@
 package Java_2.j2_lesson2;
 
+
 public class MainClass {
 
     public static void main(String[] args) {
         ProcessMatrix.processMatrix();
     }
-
 }
--- src/Java_2/j2_lesson2/MatrixUtil.java
+++ src/Java_2/j2_lesson2/MatrixUtil.java
@@ -5,35 +5,66 @@ import java.util.Arrays;
 
 public class MatrixUtil {
 
-    public static int sumMatrixItems(String[][] matrixArray, int matrixSizeLimit) {
+    public static void checkMatrixSize(String[][] strMatrix, int matrixSize) throws MyArraySizeException {
+
+        if (strMatrix.length != matrixSize) {
+            throw new MyArraySizeException(strMatrix.length);
+        }
+
+        for (int innerArr = 0; innerArr < strMatrix.length; innerArr++) {
+            if (strMatrix[innerArr].length != matrixSize) {
+                throw new MyArraySizeException(strMatrix.length, innerArr, strMatrix[innerArr].length);
+            }
+        }
+
+    }
+
+
+    public static void checkIsMatrixNumeric(String[][] strMatrix) throws MyArrayDataException {
+
+        for (int outerArr = 0; outerArr < strMatrix.length; outerArr++) {
+
+            for (int innerArr = 0; innerArr < strMatrix[outerArr].length; innerArr++) {
+                try {
+                    Integer.parseInt(strMatrix[outerArr][innerArr]);
+                } catch(NumberFormatException e) {
+                    throw new MyArrayDataException(outerArr, innerArr, strMatrix[outerArr][innerArr]);
+                }
+            }
+
+        }
+
+    }
+
+
+    public static int sumStrMatrixItems(String[][] strMatrix) {
         int sum = 0;
 
-        for (int i = 0; i < matrixSizeLimit; i++) {
-            for (int j = 0; j < matrixSizeLimit; j++) {
-                sum += Integer.parseInt(matrixArray[i][j]);
+        for (String[] strArr : strMatrix) {
+            for (String strItem : strArr) {
+                sum += Integer.parseInt(strItem);
             }
+
         }
 
         return sum;
     }
 
 
-    public static String[][] fillStrMatrixWithNumbers(String[][] strArr) {
+    public static void printStrMatrix(String[][] strMatrix) {
+        for (String[] strNumbers : strMatrix) {
+            System.out.println(Arrays.toString(strNumbers));
+        }
+    }
+
+
+    public static void fillStrMatrixWithNumbers(String[][] strArr) {
         for (int i = 0; i < strArr.length; i++) {
-            int lastNumber = (i != 0) ? Integer.parseInt(strArr[i - 1][strArr.length - 1]) : 0;
+            int lastNumber = (i != 0) ? Integer.parseInt(strArr[i - 1][strArr[i - 1].length - 1]) : 0;
             for (int j = 0; j < strArr[i].length; j++) {
                 strArr[i][j] = Integer.toString(lastNumber + 1);
                 lastNumber++;
             }
-        }
-
-        return strArr;
-    }
-
-
-    public static void printStrMatrix(String[][] strMatrix) {
-        for (String[] strNumbers : strMatrix) {
-            System.out.println(Arrays.toString(strNumbers));
         }
     }
 
