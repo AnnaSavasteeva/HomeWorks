d2a576a093258e215fe8e6d4ffa9124eeebfd4b0
AnnaSavasteeva <anna.savasteeva@gmail.com>
2021-08-10 11:59:45 +0300

    Homework 1, task 3

---
+++ src/Java_3/j3_lesson1/task_3/Apple.java
@@ -0,0 +1,15 @@
+package Java_3.j3_lesson1.task_3;
+
+public class Apple extends Fruit {
+
+    private static final float APPLE_WEIGHT = 1.0f;
+
+    public Apple() {
+        super(APPLE_WEIGHT);
+    }
+
+    public Apple(float weight) {
+        super(weight);
+    }
+
+}
---
+++ src/Java_3/j3_lesson1/task_3/Orange.java
@@ -0,0 +1,15 @@
+package Java_3.j3_lesson1.task_3;
+
+public class Orange extends Fruit {
+
+    private static final float ORANGE_WEIGHT = 1.5f;
+
+    public Orange() {
+        super(ORANGE_WEIGHT);
+    }
+
+    public Orange(float weight) {
+        super(weight);
+    }
+
+}
