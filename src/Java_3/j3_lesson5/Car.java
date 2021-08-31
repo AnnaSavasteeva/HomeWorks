00f7bf3b6834217446832222b5687f8f59043337
AnnaSavasteeva <anna.savasteeva@gmail.com>
2021-09-01 00:48:12 +0300

    2. Реализация задачи синхронизации потоков.

--- src/Java_3/j3_lesson5/Car.java
+++ src/Java_3/j3_lesson5/Car.java
@@ -1,16 +1,31 @@
 package Java_3.j3_lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
-    private static int CARS_COUNT;
+    private static int carsNameCount;
+    private static int carsFinishedCount;
+
     private final Race race;
     private final int speed;
     private final String name;
+    private int placeFinished;
 
-    public Car(Race race, int speed) {
+    private final CountDownLatch cdlStart;
+    private final CountDownLatch cdlFinish;
+    private final CyclicBarrier cb;
+
+
+    public Car(Race race, int speed, CountDownLatch cdlStart, CountDownLatch cdlFinish, CyclicBarrier cb) {
         this.race = race;
         this.speed = speed;
-        CARS_COUNT++;
-        this.name = "Участник # " + CARS_COUNT;
+        carsNameCount++;
+        this.name = "Участник # " + carsNameCount;
+        this.cdlStart = cdlStart;
+        this.cdlFinish = cdlFinish;
+        this.cb = cb;
     }
 
 
@@ -20,13 +35,20 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-        } catch (InterruptedException e) {
+            cb.await();
+            cdlStart.countDown();
+            cb.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
 
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+        placeFinished = ++carsFinishedCount;
+
+        this.cdlFinish.countDown();
     }
 
 
@@ -37,4 +59,9 @@ public class Car implements Runnable {
     public String getName() {
         return name;
     }
+
+    public int getPlaceFinished() {
+        return placeFinished;
+    }
+
 }
--- src/Java_3/j3_lesson5/MainClass.java
+++ src/Java_3/j3_lesson5/MainClass.java
@@ -1,24 +1,65 @@
 package Java_3.j3_lesson5;
 
+import java.util.concurrent.*;
+
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    private static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(CARS_COUNT);
+        CountDownLatch cdlStart = new CountDownLatch(CARS_COUNT);
+        CountDownLatch cdlFinish = new CountDownLatch(CARS_COUNT); // Для окончания гонок и объявления победителя
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT); // Для одновременного старта участников
+
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
+
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            int speed = 20 + (int) (Math.random() * 10);
+            cars[i] = new Car(race, speed, cdlStart, cdlFinish, cb);
+        }
+
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
-
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
-        }
-
         for (Car car : cars) {
-            new Thread(car).start();
+            executor.execute(car);
         }
 
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        try {
+            cdlStart.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            cdlFinish.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+            String winner = null;
+            for (Car car : cars) {
+                String carName = car.getName();
+                int place= car.getPlaceFinished();
+                System.out.printf("%s: %d место%n", carName, place);
+
+                if (place == 1) {
+                    winner = carName;
+                }
+            }
+
+            if (winner != null) {
+                System.out.println("ПОБЕДИТЕЛЬ >>> " + winner);
+            } else {
+                System.out.println("СБОЙ В НАБЛЮДЕНИИ ЗА ГОНКАМИ!");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+
     }
 
 }
